077bb844587992e35e3eb76c306366645adc15ab
norbito52 <tarnay.maxim@gmail.com>
2025-07-14 22:15:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -443,16 +443,11 @@ async def miniapp():
             background: #0F0F19;
             padding: 20px;
             padding-top: 40px;
-            height: calc(100vh - 140px);
+            min-height: calc(100vh - 140px);
             display: flex;
             flex-direction: column;
             align-items: center;
             justify-content: flex-start;
-            position: absolute;
-            top: 140px;
-            left: 0;
-            right: 0;
-            bottom: 0;
             width: 100%;
         }
         
